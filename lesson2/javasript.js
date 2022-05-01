c85a792d5fdd3807c3f0f27cf4e009a824976512
Donnovan22 <val22002@byui.edu>
2022-05-01 03:43:17 -0500

    Lesson2

---
+++ lesson2/javasript.js
@@ -0,0 +1,5 @@
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById("year").textContent = year
+
+document.getElementById("currentdate2").textContent = document.lastModified;
