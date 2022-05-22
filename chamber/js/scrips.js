b39cb8027615e7de9df7541af97b48ea4fa14836
Donnovan22 <val22002@byui.edu>
2022-05-22 02:46:14 -0500

    chamber

--- chamber/js/scrips.js
+++ chamber/js/scrips.js
@@ -16,3 +16,17 @@ let year = current.getFullYear();
 document.getElementById("year").textContent = year
 
 document.getElementById("lastModification").textContent = document.lastModified;
+
+// banner part 
+const currentDay = current.getDay();
+const message = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+if (currentDay == 1 || currentDay == 2) {
+	const el = document.createElement("div");
+	el.setAttribute("id","banner")
+	document.querySelector("#body").prepend(el);
+	const el2 = document.createElement("span");
+	el2.setAttribute("id", "message");
+	document.querySelector("#banner").appendChild(el2);
+	document.querySelector("#message").textContent = message;
+}
+
