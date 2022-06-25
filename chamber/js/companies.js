25edde950cde2793ad779406034bcb0aadbc8bb6
Donnovan22 <val22002@byui.edu>
2022-06-25 00:21:45 -0500

    directory page

--- chamber/js/companies.js
+++ chamber/js/companies.js
@@ -20,22 +20,27 @@ function buildCompaniesCards(data) {
     let p = document.createElement("p");
     let p2 = document.createElement("p");
     let p3 = document.createElement("p");
+    let p4 = document.createElement("p");
     let logo = document.createElement('img')
 
     logo.setAttribute("src", company.img);
     logo.setAttribute("alt", `logo image of ${company.name}`);
     logo.setAttribute("loading", "lazy");
 
+    name.setAttribute("class", "company-name");
+
     name.textContent = company.name;
     p.textContent = `${company.adress}`;
     p2.textContent = `${company.phoneNumber}`;
     p3.innerHTML = `<a href="${company.websiteurl}" target="_blank">${company.websiteurl}</a>`
+    p4.innerHTML = `<i>${company.memLevel}</i>`;
     
     card.append(logo);
     card.append(name);
     card.append(p);
     card.append(p2);
     card.append(p3);
+    card.append(p4);
     cards.append(card);
   });
 }
